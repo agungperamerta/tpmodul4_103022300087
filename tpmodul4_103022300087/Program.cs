87f23bf583add142193afabcdbc30e1b498d11cd
agungperamerta <gedeagung068@gmail.com>
2025-03-10 23:29:30 +0700

    Add project files.

---
+++ tpmodul4_103022300087/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103022300087
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
